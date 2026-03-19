91baadc30c9cf72c27337cd96a44666b9a0c0c0f
Vitor Luis Nonino <158179013+vitorlescola@users.noreply.github.com>
2026-03-18 22:26:50 -0300

    Add files via upload

---
+++ aula5/aula5.js
@@ -0,0 +1,9 @@
+console.log("a");
+const nota=100;
+console.log(`Sua nota é ${nota}`);
+let notaExtra=5;
+console.log(`A sua nota final é ${Number(nota+notaExtra)}`);
+
+
+let real=true;
+console.log(!real);
