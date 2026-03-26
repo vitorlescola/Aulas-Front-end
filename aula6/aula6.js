c95fcf29120214d90fdd70956439e77f97aea0df
Vitor Luis Nonino <158179013+vitorlescola@users.noreply.github.com>
2026-03-26 18:51:21 -0300

    Add files via upload

--- aula6/aula6.js
+++ aula6/aula6.js
@@ -7,12 +7,15 @@ const estados={
     gvu:"https://i.pinimg.com/736x/b2/cc/0b/b2cc0b2239d9c5b7a26693417dd9db68.jpg",
     hpy:"https://i.pinimg.com/originals/62/a3/a4/62a3a4cd36114ae37b836503d4ea77a7.gif",
     eat:"https://gifdb.com/images/high/annoyed-pichu-eating-a-fruit-pqv3dzavc9sq55hj.gif",
+    fll:"https://pbs.twimg.com/media/FERC3EzXMAssgEf.jpg",
 }
 
 let contador=0;
 let intervalo=null;
 let timeClick=null;
 let timeOut=null;
+let quant=0;
+let fin=false;
 
 function controlador(){
     if(intervalo) clearInterval(intervalo);
@@ -21,7 +24,7 @@ function controlador(){
 
         console.log("Tempo: ",contador);
 
-        if(contador>=60){
+        if(contador>=60&&fin===false){
             pk.src=estados.gvu;
         }else if(contador>=30){
             pk.src=estados.ang;
@@ -29,18 +32,38 @@ function controlador(){
     },1000);
 }
 
+function fim(){
+    fin=true;
+    pk.src=estados.fll;
+    document.getElementById("comendo").style="display:block";
+    document.getElementById("comendo").textContent="Você comeu tudo!";
+    btn.style="display:none";
+}
+
 function alimentar(){
     pk.src=estados.eat;
+    document.getElementById("comendo").style="display:block";
     contador=0;
     console.log("hm hm hm");
 
     if(timeClick) clearInterval(timeClick);
     timeClick=setTimeout(()=>{  //faz, espera o tempo, faz de novo
-        pk.src=estados.hpy;
+        if(fin===false){
+            pk.src=estados.hpy;
+        }
         timeOut=setTimeout(()=>{
+            if(fin===false){
             pk.src=estados.inc;
+            quant++;
+            document.getElementById("comendo").style="display:none";
+            document.getElementById("quant").textContent=`Quantidade comida: ${quant}/10`;
+            }
         },2000);       
     },2000);
+
+    if(quant>=10){
+        fim();
+    }
 }
 
 controlador();
---
+++ tarefa4/clickerGame.js
@@ -0,0 +1,71 @@
+let poder=0;
+let mult=1;
+let valor=20;
+let comprados=0;
+let fim=false;
+
+const estados={
+    kaio:"https://images-wixmp-ed30a86b8c4ca887773594c2.wixmp.com/f/08058935-1be6-4a21-8565-3c718332bfe3/df778et-e658f474-7b95-4dc0-8a5d-28bbb714db25.png/v1/fill/w_754,h_1059/goku_kaioken_1_png_by_12road12_df778et-pre.png?token=eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJzdWIiOiJ1cm46YXBwOjdlMGQxODg5ODIyNjQzNzNhNWYwZDQxNWVhMGQyNmUwIiwiaXNzIjoidXJuOmFwcDo3ZTBkMTg4OTgyMjY0MzczYTVmMGQ0MTVlYTBkMjZlMCIsIm9iaiI6W1t7ImhlaWdodCI6Ijw9MTc5NyIsInBhdGgiOiIvZi8wODA1ODkzNS0xYmU2LTRhMjEtODU2NS0zYzcxODMzMmJmZTMvZGY3NzhldC1lNjU4ZjQ3NC03Yjk1LTRkYzAtOGE1ZC0yOGJiYjcxNGRiMjUucG5nIiwid2lkdGgiOiI8PTEyODAifV1dLCJhdWQiOlsidXJuOnNlcnZpY2U6aW1hZ2Uub3BlcmF0aW9ucyJdfQ.QwUsOVtL-GXCV7QV8-E0aFNy-qAEya1iQ6MkqdtTwdg",
+    ssj1:"https://www.transparentpng.com/download/goku/0Hk4Ku-goku-background.png",
+    ssj2:"https://images-wixmp-ed30a86b8c4ca887773594c2.wixmp.com/f/b88f884e-d134-4606-a5ab-0eb9bcfaaa36/dg3irwx-e5a8c0de-5c31-4721-949d-6b96649ecc14.png/v1/fill/w_501,h_1594/goku_ssj2_fp_png_format_by_gogitoytb_dg3irwx-fullview.png?token=eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJzdWIiOiJ1cm46YXBwOjdlMGQxODg5ODIyNjQzNzNhNWYwZDQxNWVhMGQyNmUwIiwiaXNzIjoidXJuOmFwcDo3ZTBkMTg4OTgyMjY0MzczYTVmMGQ0MTVlYTBkMjZlMCIsIm9iaiI6W1t7ImhlaWdodCI6Ijw9MTU5NCIsInBhdGgiOiIvZi9iODhmODg0ZS1kMTM0LTQ2MDYtYTVhYi0wZWI5YmNmYWFhMzYvZGczaXJ3eC1lNWE4YzBkZS01YzMxLTQ3MjEtOTQ5ZC02Yjk2NjQ5ZWNjMTQucG5nIiwid2lkdGgiOiI8PTUwMSJ9XV0sImF1ZCI6WyJ1cm46c2VydmljZTppbWFnZS5vcGVyYXRpb25zIl19.hjegqwLdl4SklQ1d2fdREzwniaEnoSfA2pTurnBNl_Q",
+    ssj3:"https://images-wixmp-ed30a86b8c4ca887773594c2.wixmp.com/f/b88f884e-d134-4606-a5ab-0eb9bcfaaa36/dg3ijop-2bb14161-b60e-4e4c-a29a-0c74d9142e5e.png?token=eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJzdWIiOiJ1cm46YXBwOjdlMGQxODg5ODIyNjQzNzNhNWYwZDQxNWVhMGQyNmUwIiwiaXNzIjoidXJuOmFwcDo3ZTBkMTg4OTgyMjY0MzczYTVmMGQ0MTVlYTBkMjZlMCIsIm9iaiI6W1t7InBhdGgiOiIvZi9iODhmODg0ZS1kMTM0LTQ2MDYtYTVhYi0wZWI5YmNmYWFhMzYvZGczaWpvcC0yYmIxNDE2MS1iNjBlLTRlNGMtYTI5YS0wYzc0ZDkxNDJlNWUucG5nIn1dXSwiYXVkIjpbInVybjpzZXJ2aWNlOmZpbGUuZG93bmxvYWQiXX0.ylfQGuN8ySz35__4Hr7CSHDw49DBogJxZU1WQkMByLw",
+    ssj4:"https://images-wixmp-ed30a86b8c4ca887773594c2.wixmp.com/f/7c789a6c-dc0f-4c90-9977-471e89ef548d/dg1bvil-8b076e4e-a003-42c2-90d7-92ec46800035.png?token=eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJzdWIiOiJ1cm46YXBwOjdlMGQxODg5ODIyNjQzNzNhNWYwZDQxNWVhMGQyNmUwIiwiaXNzIjoidXJuOmFwcDo3ZTBkMTg4OTgyMjY0MzczYTVmMGQ0MTVlYTBkMjZlMCIsIm9iaiI6W1t7InBhdGgiOiIvZi83Yzc4OWE2Yy1kYzBmLTRjOTAtOTk3Ny00NzFlODllZjU0OGQvZGcxYnZpbC04YjA3NmU0ZS1hMDAzLTQyYzItOTBkNy05MmVjNDY4MDAwMzUucG5nIn1dXSwiYXVkIjpbInVybjpzZXJ2aWNlOmZpbGUuZG93bmxvYWQiXX0.9_ZJy01940gtawgzk2Eje8x7yGq4uCkc-2GNhg9z9Cs",
+    venc:"https://i.pinimg.com/originals/2b/66/00/2b6600305e44f6a54fc2647d59ee20ad.gif",
+}
+
+function treinar(){
+    poder+=mult;
+    document.getElementById("poder").textContent=`Poder: ${poder}`;
+    //console.log(poder);
+
+    if(Number(poder)>=valor&&!fim){
+    //console.log("a");
+    atualizar();
+}
+}
+
+function atualizar(){
+    //console.log("a");
+    document.getElementById("formas").firstElementChild.classList.replace('forma','compravel');
+    document.getElementById("formas").firstElementChild.addEventListener("click", comprar);
+}
+
+function comprar(){
+    //console.log("comprado");
+    document.getElementById("formas").firstElementChild.remove();
+    mult+=1;
+    poder-=valor;
+    document.getElementById("poder").textContent=`Poder: ${poder}`;
+    comprados++;
+
+    //document.getElementById("img").src=estados.kaio;
+
+    document.getElementById("h2").textContent=`Multiplicador: ${mult}x`;
+
+    switch(comprados){
+        case 1:
+            valor=100;
+            document.getElementById("img").src=estados.kaio;
+            break;
+        case 2:
+            valor=500;
+            document.getElementById("img").src=estados.ssj1;
+            break;
+        case 3:
+            valor=1000;
+            document.getElementById("img").src=estados.ssj2;
+            break;
+        case 4:
+            valor=2000;
+            document.getElementById("img").src=estados.ssj3;
+            break;
+        case 5:
+            document.getElementById("img").src=estados.ssj4;
+            fim=true;
+            document.getElementById("victory").style="display:block";
+            document.getElementById("vic").src=estados.venc;
+            break;
+        default:
+            valor=999999999999999;
+    }
+}
